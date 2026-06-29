9ae4d11be4a03b2c7de5b71f113425e46e988fc1
constanzabassi <cdb66@pitt.edu>
2026-06-29 17:00:00 -0400

    update config

--- Opto_sound_across_contexts/config/experiment_config.m
+++ Opto_sound_across_contexts/config/experiment_config.m
@@ -28,9 +28,9 @@ end
 
 function info = get_info_params_updated()
     % Dataset and path information
-    info.mouse_date = {'KN8-3L\2026-06-15','KN8-3L\2026-06-16','KN8-3L\2026-06-18'};
-    info.serverid = {'W:','W:','W:'};
-    info.mouseid = {1,1,1};
+    info.mouse_date = {'KN8-3L\2026-06-15','KN8-3L\2026-06-16','KN8-3L\2026-06-18','KN8-3L\2026-06-24'};
+    info.serverid = {'W:','W:','W:','W:'};
+    info.mouseid = {1,1,1,1};
     info.path_string = 'context_stim/updated'; %used get_vaid_stim_onsets_trials to get trials with imaging structure info (from context_stim/60)
     info.savepath = 'W:\Connie\results\Bassi2025\fig3\reviews';
     info.savepath_sounds = 'W:\Connie\results\Bassi2025\fig3\reviews\sounds';
